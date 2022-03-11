2ddbd2528dfea0d3dac537804b92895863a937a5
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-11 20:00:34 +0300

    Добавьте файлы проекта.

---
+++ Ex8/Program.cs
@@ -0,0 +1,16 @@
+﻿class program
+{
+    static void Main(string[] ars)
+    {
+        int patientAdmissionTime = 10;
+        int oldWomenInQueue;
+        int hours;
+        int minutes;
+
+        Console.WriteLine("Введите количество старушек в очереди:");
+        oldWomenInQueue = Convert.ToInt32(Console.ReadLine());
+        hours = (oldWomenInQueue * patientAdmissionTime) / 60;
+        minutes = (oldWomenInQueue * patientAdmissionTime) % 60;
+        Console.WriteLine("Вы должны отстоять в очереди {0} часа {1} минут", hours, minutes);
+    }
+}
